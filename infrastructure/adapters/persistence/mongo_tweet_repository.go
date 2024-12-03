3715d40ddfe26f89a70ec19ea52ebe9d52011f6e
nv <nvalfre@gmail.com>
2024-12-03 11:57:25 -0300

    Add mongo repositories

---
+++ infrastructure/adapters/persistence/mongo_tweet_repository.go
@@ -0,0 +1,46 @@
+package persistence
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"microblog/domain/models"
+	"microblog/domain/repository"
+	"microblog/infrastructure/logger"
+)
+
+type MongoTweetRepository struct {
+	Collection *mongo.Collection
+}
+
+func (r *MongoTweetRepository) Save(tweet *models.Tweet) error {
+	_, err := r.Collection.InsertOne(context.Background(), tweet)
+	return err
+}
+
+func (r *MongoTweetRepository) GetTimeline(userIDs []string) ([]models.Tweet, error) {
+	filter := bson.M{"user_id": bson.M{"$in": userIDs}}
+	opts := options.Find().SetSort(bson.D{{"created_at", -1}}).SetLimit(50)
+
+	cursor, err := r.Collection.Find(context.Background(), filter, opts)
+	if err != nil {
+		logger.Error("Error retrieving timeline", err)
+		return nil, err
+	}
+	defer cursor.Close(context.Background())
+
+	var tweets []models.Tweet
+	if err := cursor.All(context.Background(), &tweets); err != nil {
+		logger.Error("Error decoding tweets", err)
+		return nil, err
+	}
+	return tweets, nil
+}
+
+// NewMongoTweetRepository creates a new instance of MongoTweetRepository.
+func NewMongoTweetRepository(collection *mongo.Collection) repository.TweetRepository {
+	return &MongoTweetRepository{
+		Collection: collection,
+	}
+}
---
+++ infrastructure/adapters/persistence/mongo_user_repository.go
@@ -0,0 +1,66 @@
+package persistence
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"microblog/infrastructure/logger"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoUserTimelineRepository struct {
+	Collection *mongo.Collection
+}
+
+func (r *MongoUserTimelineRepository) AddFollower(followerID, userID string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	filter := bson.M{"userId": userID, "followerId": followerID}
+	update := bson.M{
+		"$setOnInsert": bson.M{
+			"createdAt": time.Now(),
+		},
+	}
+
+	_, err := r.Collection.UpdateOne(ctx, filter, update, options.Update().SetUpsert(true))
+	if err != nil {
+		logger.Error("Error adding follower to user", err, logrus.Fields{"followerID": followerID, "userID": userID})
+		return err
+	}
+
+	return nil
+}
+
+func (r *MongoUserTimelineRepository) GetFollowers(userID string) ([]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	filter := bson.M{"userId": userID}
+	cursor, err := r.Collection.Find(ctx, filter)
+	if err != nil {
+		logger.Error("Error retrieving followers", err, logrus.Fields{"userID": userID})
+		return nil, err
+	}
+
+	var followers []string
+	for cursor.Next(ctx) {
+		var rel struct {
+			FollowerId string `bson:"followerId"`
+		}
+		if err := cursor.Decode(&rel); err == nil {
+			followers = append(followers, rel.FollowerId)
+		}
+	}
+	cursor.Close(ctx)
+	return followers, nil
+}
+
+func NewMongoUserTimelineRepository(collection *mongo.Collection) *MongoUserTimelineRepository {
+	return &MongoUserTimelineRepository{
+		Collection: collection,
+	}
+}
